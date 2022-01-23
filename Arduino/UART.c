bc949fb23f6597f1a2e9d1f37ab1c4e8cbe51bd2
BetterRage <benni.schmidties@gmail.com>
2022-01-23 12:40:30 +0100

    minor UART lib changes

--- Arduino/UART.c
+++ Arduino/UART.c
@@ -1,8 +1,8 @@
 #include "UART.h"
 
-void init_usart(uint32_t baud)
+void init_usart9600()
 {
-    UBRR0L = (16000000ul/(16ul*baud))-1;
+    UBRR0L = 103;
     UBRR0H = 0;
     UCSR0B |= (1<<TXEN0) | (1<<RXEN0)|(1<<RXCIE0);           // Sender und Empfänger aktivieren
     UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);         // 8-Bit Übertragung
@@ -15,10 +15,9 @@ void _putch(char ch)
     UDR0 = ch;
 }
 
-void _puts(char st[])
+void _puts(char st[],uint8_t len)
 {
-    uint8_t i=0;
-    while(st[i])
+    for(int i=0;i<len;i++)
         _putch(st[i++]);
 }
 
@@ -33,20 +32,11 @@ void _gets(char* st)
     do
     {
         *st=_getch();
-    } while (*st++);  // die Funktion WriteLine von C# schickt einen String,
+    } while (*st++); 
 }
 
 void _newline()
 {
     _putch(13);   // CR
     _putch(10);   // LF
-}
-
-void itoa8(uint16_t val,char *stringa)
-{
-    stringa[0]=(val%10000)/1000+48;
-    stringa[1]=(val%1000)/100+48;
-    stringa[2]=(val%100)/10+48;
-    stringa[3]=(val%10)+48;
-    stringa[4]=0;
 }
--- Arduino/UART.h
+++ Arduino/UART.h
@@ -1,10 +1,10 @@
 #include <avr/io.h>
 
-void init_usart(uint32_t baud);
+void init_usart9600();
 
 void _putch(char ch);
 
-void _puts(char st[]);
+void _puts(char st[],uint8_t len);
 
 char _getch(void);
 
